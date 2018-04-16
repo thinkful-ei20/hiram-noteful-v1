ca4babf6213471d4ec5c248a7b12c1736f888498
Hiram Cruz <hiram0101@gmail.com>
2018-04-16 15:00:28 -0400

    completed challenge

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
-'use strict';
+`use strict`;
 
-const data = require('./db/notes');
+const express = require(`express`);
+const data = require(`./db/notes`);
 
-console.log('Hello Noteful!');
+const app = express()
+app.use(express.static(`public`))
 
-// INSERT EXPRESS APP CODE HERE...
+app.get(`/api/notes`, (req, res) => {
+  const notes = data.filter((val) => {
+    if (req.query.searchTerm) {
+      return (
+        val.title.toLowerCase().includes(req.query.searchTerm.toLowerCase()) ||
+        val.content.toLowerCase().includes(req.query.searchTerm.toLowerCase())
+      )
+    }
+    return true
+  })
+  res.json(notes)
+})
+
+app.get(`/api/notes/:id`, (req, res) => {
+  const note = data.find((val) => val.id === Number(req.params.id))
+  res.json(note)
+})
+
+app.listen(3000, function() {
+  console.info(`Server listening on ${this.address().port}`)
+}).on(`error`, err => {
+  console.error(err)
+})
