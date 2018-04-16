47caa955e73de6506029cdf202c5ab2c90dee88e
Hiram Cruz <hiram0101@gmail.com>
2018-04-16 15:01:03 -0400

    formatting

--- server.js
+++ server.js
@@ -3,29 +3,31 @@
 const express = require(`express`);
 const data = require(`./db/notes`);
 
-const app = express()
-app.use(express.static(`public`))
+const app = express();
+app.use(express.static(`public`));
 
 app.get(`/api/notes`, (req, res) => {
-  const notes = data.filter((val) => {
+  const notes = data.filter(val => {
     if (req.query.searchTerm) {
       return (
         val.title.toLowerCase().includes(req.query.searchTerm.toLowerCase()) ||
         val.content.toLowerCase().includes(req.query.searchTerm.toLowerCase())
-      )
+      );
     }
-    return true
-  })
-  res.json(notes)
-})
+    return true;
+  });
+  res.json(notes);
+});
 
 app.get(`/api/notes/:id`, (req, res) => {
-  const note = data.find((val) => val.id === Number(req.params.id))
-  res.json(note)
-})
+  const note = data.find(val => val.id === Number(req.params.id));
+  res.json(note);
+});
 
-app.listen(3000, function() {
-  console.info(`Server listening on ${this.address().port}`)
-}).on(`error`, err => {
-  console.error(err)
-})
+app
+  .listen(3000, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  })
+  .on(`error`, err => {
+    console.error(err);
+  });
